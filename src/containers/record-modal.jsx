482dbf98d1ebe83088be03f8fffa287f81c3df78
Paul Kaplan <pkaplan@media.mit.edu>
2017-06-29 09:40:34 -0400

    HACK download wav

--- src/containers/record-modal.jsx
+++ src/containers/record-modal.jsx
@@ -80,6 +80,23 @@ class RecordModal extends React.Component {
                     name: `recording ${this.props.vm.editingTarget.sprite.sounds.length}`
                 };
 
+                // Download project data into a file - create link element,
+                // simulate click on it, and then remove it.
+                const saveLink = document.createElement('a');
+                document.body.appendChild(saveLink);
+
+                const data = new Blob([wavBuffer], {type: 'audio/wav'});
+                const url = window.URL.createObjectURL(data);
+                saveLink.href = url;
+
+                // File name: project-DATE-TIME
+                const date = new Date();
+                const timestamp = `${date.toLocaleDateString()}-${date.toLocaleTimeString()}`;
+                saveLink.download = `sound-${timestamp}.wav`;
+                saveLink.click();
+                window.URL.revokeObjectURL(url);
+                document.body.removeChild(saveLink);
+
                 // Load the encoded .wav into the storage cache
                 const storage = this.props.vm.runtime.storage;
                 storage.builtinHelper.cache(
